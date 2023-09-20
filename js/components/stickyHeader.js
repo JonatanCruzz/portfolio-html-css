ba4e470037c28e87b898b94dd5a9e74c367c678f
JonatanCruzz <91216157+JonatanCruzz@users.noreply.github.com>
2023-09-20 17:25:16 -0400

    feat: text-animate and animating section home

--- js/components/stickyHeader.js
+++ js/components/stickyHeader.js
@@ -18,6 +18,12 @@ function stickyHeader() {
                     links.classList.remove('active');
                     document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
                 });
+
+                // active sections for animation on scroll
+                section.classList.add('show-animate');
+            }
+            else {
+                section.classList.remove('show-animate');
             }
         });
 
