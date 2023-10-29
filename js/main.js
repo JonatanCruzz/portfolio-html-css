09a92ec1086e691841dd6bf5d3ab1fa3b88f8035
JonatanCruzz <jonatanalexanderdiaz@gmail.com>
2023-10-29 16:32:24 -0400

    feat: has been added a new section called personal projects with their styles

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 import stickyHeader from "./components/stickyHeader.js";
 import toggleIconNavbar from "./components/toggleIconNavbar.js";
 import readMore from "./components/readMore.js";
-import {sendEmail, reset} from "./components/sendEmail.js";
 
 //toggle icon navbar
 toggleIconNavbar();
